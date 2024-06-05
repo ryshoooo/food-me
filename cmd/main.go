3b2db779e9a089196e458c61466b19ecf4adde03
Richard Nemeth <ryshoooo@gmail.com>
2024-06-05 07:39:08 +0200

    feat: base auth

--- cmd/main.go
+++ cmd/main.go
@@ -7,47 +7,6 @@ import (
 	foodme "github.com/ryshoooo/food-me/internal"
 )
 
-// func copyAndPrint(source string, dst io.Writer, src io.Reader) {
-// 	buf := make([]byte, 64000)
-// 	// startup [0 0 0 8 4 210 22 47]
-// 	for {
-// 		n, err := src.Read(buf)
-// 		if err != nil {
-// 			if err != io.EOF {
-// 				log.Printf("Error reading from source: %v\n", err)
-// 			}
-// 			return
-// 		}
-
-// 		// Print the data read from src
-// 		fmt.Printf("Data from %s: %v %s\n", source, buf[:n], buf[:n])
-
-// 		// Write the data to the destination
-// 		if _, err := dst.Write(buf[:n]); err != nil {
-// 			log.Printf("Error writing to destination: %v\n", err)
-// 			return
-// 		}
-// 	}
-// }
-
-// func handleConnection(src net.Conn, destAddr string) {
-// 	// Establish a connection to the destination address
-// 	dest, err := net.Dial("tcp", destAddr)
-// 	if err != nil {
-// 		log.Printf("Unable to connect to destination: %v\n", err)
-// 		src.Close()
-// 		return
-// 	}
-// 	// Close both connections when this function exits
-// 	defer fmt.Println("Closing connections")
-// 	defer src.Close()
-// 	defer dest.Close()
-
-// 	// // Use io.Copy to forward data between the source and destination
-// 	go copyAndPrint("PROXY", dest, src)
-// 	copyAndPrint("DATABASE", src, dest)
-// }
-
 func main() {
 	conf, err := foodme.NewConfiguration(os.Args)
 	if err != nil {
--- internal/handlers/postgres.go
+++ internal/handlers/postgres.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"io"
 	"net"
 
 	"github.com/sirupsen/logrus"
@@ -23,11 +24,84 @@ func NewPostgresHandler(address, username, password string, logger *logrus.Logge
 }
 
 func (h *PostgresHandler) Handle(conn net.Conn) error {
-	_, err := net.Dial("tcp", h.Address)
+	defer conn.Close()
+
+	destination, err := net.Dial("tcp", h.Address)
 	if err != nil {
 		h.Logger.Errorf("Unable to connect to destination: %v", err)
-		conn.Close()
+		return err
+	}
+	defer destination.Close()
+	go io.Copy(conn, destination)
+
+	err = h.Startup(conn, destination)
+	if err != nil {
+		h.Logger.Errorf("Error on startup: %v", err)
+		return err
+	}
+
+	err = h.Authenticate(conn, destination)
+	if err != nil {
+		h.Logger.Errorf("Error on authentication: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (h *PostgresHandler) Startup(conn, dest net.Conn) error {
+	startup, err := h.Read(conn, 8)
+	if err != nil {
+		h.Logger.Errorf("Error reading startup packet from source: %v", err)
+		return err
+	}
+	h.Logger.Debugf("Read startup packet from source: %v", startup)
+	return h.Write(dest, startup)
+}
+
+func (h *PostgresHandler) Write(dest net.Conn, data []byte) error {
+	h.Logger.Debugf("Writing data to destination: %v", data)
+	n, err := dest.Write(data)
+	if err != nil || n != len(data) {
+		h.Logger.Errorf("Error writing to destination: %v", err)
 		return err
 	}
 	return nil
+}
+
+func (h *PostgresHandler) Read(conn net.Conn, size int) ([]byte, error) {
+	h.Logger.Debugf("Reading %v bytes from source", size)
+	buff := make([]byte, size)
+	n, err := conn.Read(buff)
+	if err != nil || n != size {
+		h.Logger.Errorf("Error reading from source: %v", err)
+		return nil, err
+	}
+	return buff, nil
+}
+
+func (h *PostgresHandler) Authenticate(conn, dest net.Conn) error {
+	h.Logger.Info("Commencing authentication")
+	sizebuff, err := h.Read(conn, 4)
+	if err != nil {
+		h.Logger.Errorf("Error reading authentication size from source: %v", err)
+		return err
+	}
+
+	h.Logger.Debugf("Sizebuff %v", sizebuff)
+	size := calculatePacketSize(sizebuff)
+	h.Logger.Debugf("Computed size %v", size)
+
+	auth, err := h.Read(conn, size-4)
+	if err != nil {
+		h.Logger.Errorf("Error reading authentication packet from source: %v", err)
+		return err
+	}
+	h.Logger.Debugf("Read authentication packet from source: %v (%s)", auth, auth)
+
+	return nil
+}
+
+func calculatePacketSize(sizebuff []byte) int {
+	return int(sizebuff[0])<<24 | int(sizebuff[1])<<16 | int(sizebuff[2])<<8 | int(sizebuff[3])
 }
