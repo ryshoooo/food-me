1b3022a0dabd5304825936e9441fac7291643f4a
Richard Nemeth <ryshoooo@gmail.com>
2024-06-09 16:11:34 +0200

    chore: example working

--- examples/postgres-keycloak/example.py
+++ examples/postgres-keycloak/example.py
@@ -1,7 +1,7 @@
 import keycloak
 import psycopg2
 import pyodbc
-import base64
+import requests
 
 # Get Keycloak token
 oid = keycloak.KeycloakOpenID(
@@ -9,10 +9,8 @@ oid = keycloak.KeycloakOpenID(
 )
 tokens = oid.token(username="admin", password="admin")
 
-user = base64.b64encode(
-    f"access_token={tokens['access_token']};refresh_token={tokens['refresh_token']}".encode()
-).decode()
-# Connect to PostgreSQL
+# 1. Direct connection with libpq
+user = f"access_token={tokens['access_token']};refresh_token={tokens['refresh_token']}"
 dsn = f"dbname=postgres host=localhost port=5432 user={user}"
 conn = psycopg2.connect(dsn)
 
@@ -20,9 +18,22 @@ conn = psycopg2.connect(dsn)
 cur = conn.cursor()
 cur.execute("SELECT current_user;")
 print(cur.fetchone())
-cur.execute("SELECT tablename FROM pg_catalog.pg_tables")
-print(cur.fetchall())
 
+# 2. Create connection using the API
+resp = requests.post(
+    "http://localhost:10000/connection",
+    json={"access_token": tokens["access_token"], "refresh_token": tokens["refresh_token"]},
+).json()
+user = resp["username"]
+dsn = f"dbname=postgres host=localhost port=5432 user={user}"
+conn = psycopg2.connect(dsn)
+
+# Execute a query
+cur = conn.cursor()
+cur.execute("SELECT current_user;")
+print(cur.fetchone())
+
+# 3. Using PyODBC - Only works with the connection API
 # Pyodbc
 conn_str = (
     "DRIVER={PostgreSQL Unicode};"
@@ -32,3 +43,8 @@ conn_str = (
     "PORT=5432;"
 )
 cnxn = pyodbc.connect(conn_str)
+
+# Execute a query
+c = cnxn.cursor()
+c.execute("SELECT current_user;")
+print(c.fetchone())
