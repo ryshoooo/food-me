7474823d40d040e3e9117fb138e461240d930c49
Richard Nemeth <ryshoooo@gmail.com>
2024-06-09 12:10:13 +0200

    chore: example update

--- examples/postgres-keycloak/example.py
+++ examples/postgres-keycloak/example.py
@@ -1,5 +1,7 @@
 import keycloak
 import psycopg2
+import pyodbc
+import base64
 
 # Get Keycloak token
 oid = keycloak.KeycloakOpenID(
@@ -7,8 +9,11 @@ oid = keycloak.KeycloakOpenID(
 )
 tokens = oid.token(username="admin", password="admin")
 
+user = base64.b64encode(
+    f"access_token={tokens['access_token']};refresh_token={tokens['refresh_token']}".encode()
+).decode()
 # Connect to PostgreSQL
-dsn = f"dbname=postgres host=localhost port=5432 user=access_token={tokens['access_token']};refresh_token={tokens['refresh_token']}"
+dsn = f"dbname=postgres host=localhost port=5432 user={user}"
 conn = psycopg2.connect(dsn)
 
 # Execute a query
@@ -17,3 +22,13 @@ cur.execute("SELECT current_user;")
 print(cur.fetchone())
 cur.execute("SELECT tablename FROM pg_catalog.pg_tables")
 print(cur.fetchall())
+
+# Pyodbc
+conn_str = (
+    "DRIVER={PostgreSQL Unicode};"
+    "DATABASE=postgres;"
+    f"UID={user};"
+    "SERVER=localhost;"
+    "PORT=5432;"
+)
+cnxn = pyodbc.connect(conn_str)
