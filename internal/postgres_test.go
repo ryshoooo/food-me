41cebb67577b8e86f71d7e95543d21997ddb71b2
Richard Nemeth <ryshoooo@gmail.com>
2024-07-22 21:41:13 +0100

    test: pg startup full

--- internal/postgres_test.go
+++ internal/postgres_test.go
@@ -15,6 +15,7 @@ type MockNetConn struct {
 	FailWrite   bool
 	Responses   [][]byte
 	ResponseIdx int
+	Writes      [][]byte
 }
 
 func (m *MockNetConn) Close() error {
@@ -34,7 +35,7 @@ func (m *MockNetConn) Read(buff []byte) (int, error) {
 		return 0, fmt.Errorf("read failed")
 	} else {
 		if len(buff) != len(m.Responses[m.ResponseIdx]) {
-			return 0, fmt.Errorf("buffer size mismatch")
+			return 0, fmt.Errorf("buffer size mismatch: %d != %d", len(buff), len(m.Responses[m.ResponseIdx]))
 		}
 		copy(buff, m.Responses[m.ResponseIdx])
 		m.ResponseIdx++
@@ -58,6 +59,7 @@ func (m *MockNetConn) Write(buff []byte) (int, error) {
 	if m.FailWrite {
 		return 0, fmt.Errorf("write failed")
 	} else {
+		m.Writes = append(m.Writes, buff)
 		return len(buff), nil
 	}
 }
@@ -89,7 +91,7 @@ func TestPGHandlerStartup(t *testing.T) {
 	mc = &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {}}}
 	handler.client = mc
 	res, err = handler.startup()
-	assert.Error(t, err, "buffer size mismatch")
+	assert.Error(t, err, "buffer size mismatch: 4 != 0")
 	assert.DeepEqual(t, res, []byte{})
 
 	// Fail write to upstream
@@ -107,7 +109,7 @@ func TestPGHandlerStartup(t *testing.T) {
 	handler.client = mc
 	handler.upstream = mu
 	res, err = handler.startup()
-	assert.Error(t, err, "buffer size mismatch")
+	assert.Error(t, err, "buffer size mismatch: 1 != 0")
 	assert.DeepEqual(t, res, []byte{})
 
 	// Bad PG response to startup
@@ -117,5 +119,85 @@ func TestPGHandlerStartup(t *testing.T) {
 	handler.upstream = mu
 	res, err = handler.startup()
 	assert.Error(t, err, "unexpected response from upstream: [81]")
+	assert.DeepEqual(t, res, []byte{})
+
+	// N response - client write fail
+	mc = &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {1, 2, 3, 4}}, FailWrite: true}
+	mu = &MockNetConn{Responses: [][]byte{[]byte("N")}}
+	handler.client = mc
+	handler.upstream = mu
+	res, err = handler.startup()
+	assert.Error(t, err, "write failed")
+	assert.DeepEqual(t, res, []byte{})
+
+	// N response - client read fail
+	mc = &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {1, 2, 3, 4}, {}}}
+	mu = &MockNetConn{Responses: [][]byte{[]byte("N")}}
+	handler.client = mc
+	handler.upstream = mu
+	res, err = handler.startup()
+	assert.Error(t, err, "buffer size mismatch: 4 != 0")
+	assert.DeepEqual(t, res, []byte{})
+
+	// N response - OK
+	mc = &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {1, 2, 3, 4}, {0, 0, 0, 1}}}
+	mu = &MockNetConn{Responses: [][]byte{[]byte("N")}}
+	handler.client = mc
+	handler.upstream = mu
+	res, err = handler.startup()
+	assert.NilError(t, err)
+	assert.DeepEqual(t, res, []byte{0, 0, 0, 1})
+}
+
+func TestPGHandlerStartupTLS(t *testing.T) {
+	logger := logrus.StandardLogger()
+	handler := NewPostgresHandler("addr", "user", "pwd", nil, logger, false, false, false, nil, "clientId", "clientSecret", "token-url", "userinfo-url", false, nil, "", nil, false, "", "", false, "", false)
+
+	// Client write fail
+	mc := &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {1, 2, 3, 4}}, FailWrite: true}
+	mu := &MockNetConn{Responses: [][]byte{[]byte("S")}}
+	handler.client = mc
+	handler.upstream = mu
+	res, err := handler.startup()
+	assert.Error(t, err, "write failed")
+	assert.DeepEqual(t, res, []byte{})
+
+	// TLS disabled, OK
+	mc = &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {1, 2, 3, 4}, {0, 0, 0, 1}}}
+	mu = &MockNetConn{Responses: [][]byte{[]byte("S")}}
+	handler.client = mc
+	handler.upstream = mu
+	res, err = handler.startup()
+	assert.NilError(t, err)
+	assert.DeepEqual(t, res, []byte{0, 0, 0, 1})
+
+	// TLS enabled, fail to load keys
+	handler.TLSEnabled = true
+	mc = &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {1, 2, 3, 4}, {0, 0, 0, 1}}}
+	mu = &MockNetConn{Responses: [][]byte{[]byte("S")}}
+	handler.client = mc
+	handler.upstream = mu
+	res, err = handler.startup()
+	assert.Error(t, err, "open : no such file or directory")
+	assert.DeepEqual(t, res, []byte{})
+
+	// TLS OK, fail to write to client
+	handler.TLSCertificateFile = "../data/cert.pem"
+	handler.TLSCertificateKeyFile = "../data/key.pem"
+	mc = &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {1, 2, 3, 4}, {0, 0, 0, 1}}, FailWrite: true}
+	mu = &MockNetConn{Responses: [][]byte{[]byte("S")}}
+	handler.client = mc
+	handler.upstream = mu
+	res, err = handler.startup()
+	assert.Error(t, err, "write failed")
+	assert.DeepEqual(t, res, []byte{})
+
+	// TLS OK (expected failure as tests dont do TLS exchange)
+	mc = &MockNetConn{Responses: [][]byte{{0, 0, 0, 8}, {1, 2, 3, 4}, {0}}}
+	mu = &MockNetConn{Responses: [][]byte{[]byte("S")}}
+	handler.client = mc
+	handler.upstream = mu
+	res, err = handler.startup()
+	assert.Error(t, err, "buffer size mismatch: 576 != 1")
 	assert.DeepEqual(t, res, []byte{})
 }
