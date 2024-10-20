48d6771e535b049637f9036f0866b8fabf55a46f
Richard Nemeth <ryshoooo@gmail.com>
2024-10-20 11:29:56 +0200

    test: full http pa

--- internal/permission_agent_http_test.go
+++ internal/permission_agent_http_test.go
@@ -33,3 +33,119 @@ func TestPermissionAgentQuery(t *testing.T) {
 	assert.NilError(t, err)
 	assert.Equal(t, string(resp), "hello")
 }
+
+func TestPermissionAgentDDLQuery(t *testing.T) {
+	httpClient := &MockHttpClient{}
+	pa := &HTTPPermissionAgent{client: httpClient}
+
+	_, err := pa.ddlQuery("op", map[string]interface{}{"a": map[interface{}]bool{nil: false}})
+	assert.Error(t, err, "failed to marshal json payload: json: unsupported type: map[interface {}]bool")
+
+	_, err = pa.ddlQuery("op", map[string]interface{}{"a": "b"})
+	assert.Error(t, err, "failed to query ddl: failed to execute request: failed to do request")
+
+	httpClient.DoSucceed = true
+	httpClient.StatusCode = http.StatusOK
+	_, err = pa.ddlQuery("op", map[string]interface{}{"a": "b"})
+	assert.Error(t, err, "failed to unmarshal response body: unexpected end of JSON input")
+
+	httpClient.Response = `{"allowed": true}`
+	resp, err := pa.ddlQuery("op", map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.Assert(t, resp.Allowed)
+
+	httpClient.Response = `{"allowed": false}`
+	resp, err = pa.ddlQuery("op", map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.Assert(t, !resp.Allowed)
+}
+
+func TestPermissionAgentCreateAllowed(t *testing.T) {
+	httpClient := &MockHttpClient{}
+	pa := &HTTPPermissionAgent{client: httpClient}
+
+	err := pa.SetCreateAllowed(map[string]interface{}{"a": "b"})
+	assert.Error(t, err, "failed to query ddl: failed to query ddl: failed to execute request: failed to do request")
+
+	httpClient.DoSucceed = true
+	httpClient.StatusCode = http.StatusOK
+	httpClient.Response = `{"allowed": true}`
+	err = pa.SetCreateAllowed(map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.Assert(t, pa.CreateAllowed())
+
+	httpClient.Response = `{"allowed": false}`
+	err = pa.SetCreateAllowed(map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.Assert(t, !pa.CreateAllowed())
+}
+
+func TestPermissionAgentUpdateAllowed(t *testing.T) {
+	httpClient := &MockHttpClient{}
+	pa := &HTTPPermissionAgent{client: httpClient}
+
+	err := pa.SetUpdateAllowed(map[string]interface{}{"a": "b"})
+	assert.Error(t, err, "failed to query ddl: failed to query ddl: failed to execute request: failed to do request")
+
+	httpClient.DoSucceed = true
+	httpClient.StatusCode = http.StatusOK
+	httpClient.Response = `{"allowed": true}`
+	err = pa.SetUpdateAllowed(map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.Assert(t, pa.UpdateAllowed())
+
+	httpClient.Response = `{"allowed": false}`
+	err = pa.SetUpdateAllowed(map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.Assert(t, !pa.UpdateAllowed())
+}
+
+func TestPermissionAgentDeleteAllowed(t *testing.T) {
+	httpClient := &MockHttpClient{}
+	pa := &HTTPPermissionAgent{client: httpClient}
+
+	err := pa.SetDeleteAllowed(map[string]interface{}{"a": "b"})
+	assert.Error(t, err, "failed to query ddl: failed to query ddl: failed to execute request: failed to do request")
+
+	httpClient.DoSucceed = true
+	httpClient.StatusCode = http.StatusOK
+	httpClient.Response = `{"allowed": true}`
+	err = pa.SetDeleteAllowed(map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.Assert(t, pa.DeleteAllowed())
+
+	httpClient.Response = `{"allowed": false}`
+	err = pa.SetDeleteAllowed(map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.Assert(t, !pa.DeleteAllowed())
+}
+
+func TestPermissionAgentSelectFilters(t *testing.T) {
+	httpClient := &MockHttpClient{}
+	pa := &HTTPPermissionAgent{client: httpClient}
+
+	_, err := pa.SelectFilters("table", "alias", map[string]interface{}{"a": map[interface{}]bool{nil: false}})
+	assert.Error(t, err, "failed to marshal json payload: json: unsupported type: map[interface {}]bool")
+
+	_, err = pa.SelectFilters("table", "alias", map[string]interface{}{"a": "b"})
+	assert.Error(t, err, "failed to query select filters: failed to execute request: failed to do request")
+
+	httpClient.DoSucceed = true
+	httpClient.StatusCode = http.StatusOK
+	_, err = pa.SelectFilters("table", "alias", map[string]interface{}{"a": "b"})
+	assert.Error(t, err, "failed to unmarshal response body: unexpected end of JSON input")
+
+	httpClient.Response = `{"allowed": false}`
+	_, err = pa.SelectFilters("table", "alias", map[string]interface{}{"a": "b"})
+	assert.Error(t, err, "permission denied to access table table")
+
+	httpClient.Response = `{"allowed": true}`
+	resp, err := pa.SelectFilters("table", "alias", map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.DeepEqual(t, resp, &SelectFilters{WhereFilters: []string{}, JoinFilters: []*JoinFilter{}})
+
+	httpClient.Response = `{"allowed": true, "filters": {"whereFilters": ["a = 1"], "joinFilters": [{"tableName": "table", "conditions": "a = b"}]}}`
+	resp, err = pa.SelectFilters("table", "alias", map[string]interface{}{"a": "b"})
+	assert.NilError(t, err)
+	assert.DeepEqual(t, resp, &SelectFilters{WhereFilters: []string{"a = 1"}, JoinFilters: []*JoinFilter{{TableName: "table", Conditions: "a = b"}}})
+}
